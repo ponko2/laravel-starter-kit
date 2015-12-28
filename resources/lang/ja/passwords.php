ae4505d25ca9cdb0656dfe96d9f822e9373cf459
Takahito Nakano <kano@ponko2.jp>
2015-12-28 18:17:18 +0900

    日本語の言語ファイルを生成

---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'パスワードは最低６文字で、確認項目と一致している必要があります。',
+    'reset' => 'パスワードをリセットしました。',
+    'sent' => 'パスワードリセットメールを送信しました。',
+    'token' => 'このパスワードリセットトークンは無効です。',
+    'user' => 'メールアドレスに一致するユーザーは存在していません。',
+
+];
