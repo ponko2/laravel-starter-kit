3dc5d9275af8f51098384b4e02f9da2fa6bc3ba6
Takahito Nakano <kano@ponko2.jp>
2016-04-23 01:50:42 +0900

    Laravel v5.2.29までの変更内容をマージ

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 }
